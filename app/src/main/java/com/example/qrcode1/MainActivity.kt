5bd3d7f6f4f4311899916408af7fc3d1f44b4291
EragonBrisingr <geometrydasher26@gmail.com>
2024-05-20 23:06:53 +0300

    Weclome page added

--- app/src/main/java/com/example/qrcode1/MainActivity.kt
+++ app/src/main/java/com/example/qrcode1/MainActivity.kt
@@ -14,40 +14,11 @@ import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var QRCode: ImageView
-    private lateinit var Data: EditText
-    private lateinit var qrGenerator: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialize the views
-        QRCode = findViewById(R.id.QRCode)
-        Data = findViewById(R.id.Data)
-        qrGenerator = findViewById(R.id.QRGenerator)
 
-        qrGenerator.setOnClickListener {
-            val data = Data.text.toString().trim()
-            if (data.isEmpty()) {
-                Toast.makeText(this, "Please enter data", Toast.LENGTH_SHORT).show()
-            } else {
-                val qrCodeWriter = QRCodeWriter()
-                try {
-                    val bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, 512, 512)
-                    val width = bitMatrix.width
-                    val height = bitMatrix.height
-                    val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-                    for (x in 0 until width) {
-                        for (y in 0 until height) {
-                            bmp.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
-                        }
-                    }
-                    QRCode.setImageBitmap(bmp)
-                } catch (e: WriterException) {
-                    e.printStackTrace()
-                }
-            }
-        }
     }
 }
