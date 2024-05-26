6dd5e1c7b97508f17dbd57b67913e0c7d002deba
Hamzeh Zaza <hamzehzaza2@gmail.com>
2024-05-26 22:26:19 +0300

    Registration page working completely with the DB

--- app/src/main/java/com/example/qrcode1/Registration.kt
+++ app/src/main/java/com/example/qrcode1/Registration.kt
@@ -19,15 +19,52 @@ import com.google.zxing.qrcode.QRCodeWriter
 
 class Registration : AppCompatActivity() {
 
-    //private lateinit var auth: FirebaseAuth
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var password_confirm: EditText
+    private lateinit var registerButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        auth = FirebaseAuth.getInstance()
 
-//        fun register() {
-//            auth = FirebaseAuth.getInstance()
-//        }
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        password_confirm = findViewById(R.id.confirm_password)
+        registerButton = findViewById(R.id.signup)
+
+        registerButton.setOnClickListener {
+            val email = email.text.toString()
+            val password = password.text.toString()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                if(password == password_confirm.text.toString()) {
+                    registerUser(email, password)
+                } else {
+                    Toast.makeText(this, "The Passwords do not match", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Registration successful
+                    Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+                    // Optionally, redirect to another activity or clear the input fields
+                } else {
+                    // Registration failed
+                    Toast.makeText(
+                        this,
+                        "Registration failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
